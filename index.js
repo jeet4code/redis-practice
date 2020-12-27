729e12578b6074dd42bd158cb2998356d990c163
Jeetendra chauhan <jeet4code@gmail.com>
2020-12-27 15:19:28 +0530

    basic setup

---
+++ index.js
@@ -0,0 +1,61 @@
+const redis = require("redis");
+const express = require("express");
+const { promisify } = require("util");
+const request = promisify(require("request"));
+
+const app = express();
+
+const redisClient = redis.createClient();
+
+redisClient.on("error", function(error) {
+    console.error(error);
+});
+
+redisClient.get = promisify(redisClient.get);
+// redisClient.hget = promisify(redisClient.hget);
+
+app.get('/', async(req, res, next) => {
+    const cachedData = await redisClient.get('allUsers');
+    if(cachedData) {
+        console.log("From CACHE");
+        res.json(JSON.parse(cachedData));
+        return;
+    }
+
+    const users = await request("https://jsonplaceholder.typicode.com/users");
+    redisClient.set('allUsers', users.body, "EX", 10); // users.body are string so no need to stringify 
+    console.log("From DB");
+    res.json(JSON.parse(users.body));
+});
+
+app.get('/flushdb', (req, res) => {
+    redisClient.flushdb((err, success) => {
+        if(err) res.send(err);
+        res.send("Cleared all Data");
+    });
+})
+
+app.listen(3000, () => {
+    console.log("application running at: http://localhot:3000");
+})
+
+// const client = redis.createClient();
+
+// client.get = promisify(client.get);
+// client.set = promisify(client.set);
+
+// client.hget = promisify(client.hget);
+// client.hset = promisify(client.hset);
+
+
+// async function init() {
+//     // await client.set("key", "value");
+//     // const value = await client.get("key");
+
+//     // console.log(value);
+
+//     await client.hset("hashkey", "foo", '{"name": "jeetendra"}');
+//     const hashValue = await client.hget("hashkey", "foo");
+//     console.log("hashValue", JSON.parse(hashValue));
+// }
+// init();
---
+++ readme.md
@@ -0,0 +1,10 @@
+#Redis example to cache data
+
+### Local changes for local env.
+
+1. ##### set enviournment path to redis folder
+
+2. ##### Run local server
+    ```bash
+    redis-server "c:\Program Files\Redis\redis.windows.conf"
+    ```
