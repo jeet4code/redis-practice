9968426b9eb90a1a7c355275d09c21bf35207fe0
Jeetendra chauhan <jeet4code@gmail.com>
2020-12-29 16:32:24 +0530

    added helmet cors and compression packages

--- src/index.js
+++ src/index.js
@@ -4,16 +4,26 @@ require('./db-connection');
 const bodyParser = require("body-parser");
 const userRoute = require("./routes/user");
 const redisClient = require("./redis-connection");
+const compression = require('compression');
+const helmet = require('helmet');
+const cors = require("cors");
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname,'views'));
 
+app.use(helmet());
+app.use(compression());
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'assets')));
 
 app.get('/', async(req, res, next) => {
     res.redirect('/user');
 });
+
+app.use("/user", userRoute);
 
 app.get('/flushcache', (req, res) => {
     redisClient.flushdb((err, success) => {
@@ -21,8 +31,6 @@ app.get('/flushcache', (req, res) => {
         res.send("Cleared all Data");
     });
 });
-
-app.use("/user", userRoute);
 
 app.listen(3000, () => {
     console.log("application running at: http://localhot:3000");
